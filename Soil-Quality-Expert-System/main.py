f97c430b67dae0ad37c483a7c95517edcbd534ae
Tyto1106 <149225464+Tyto1106@users.noreply.github.com>
2024-10-30 21:48:12 +0700

    Update main.py

--- Soil-Quality-Expert-System/main.py
+++ Soil-Quality-Expert-System/main.py
@@ -25,7 +25,7 @@ def get_cf_pengguna(choice):
 # Fungsi untuk membaca knowledge base dari file di direktori data
 def load_knowledge_base_from_file():
     knowledge_base = {}
-    # Tentukan path absolut untuk file knowledge base dalam direktori "data"
+    
     file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "knowledge_bases.txt"))
     
     print(f"Mencoba memuat file knowledge base dari path: {file_path}")
@@ -39,11 +39,11 @@ def load_knowledge_base_from_file():
                     continue
                 if line.startswith("K"):  # Baris yang memulai kode penyakit
                     kode_kerusakan, penyakit_name = line.split(" - ")
-                    knowledge_base[kode_kerusakan] = {'name': penyakit_name, 'symptoms': {}, 'solution': ''}
+                    knowledge_base[kode_kerusakan] = {'name': penyakit_name, 'diseases': {}, 'solution': ''}
                 elif line.startswith("T"):  # Baris yang memulai kode gejala
                     gejala_code, rest = line.split(": ")
-                    gejala_name, weight = rest.split(" - ")
-                    knowledge_base[kode_kerusakan]['symptoms'][gejala_code] = {'name': gejala_name, 'weight': float(weight)}
+                    gejala_name, berat = rest.split(" - ")
+                    knowledge_base[kode_kerusakan]['diseases'][gejala_code] = {'name': gejala_name, 'berat': float(berat)}
                 elif line.startswith("Solusi:"):  # Baris solusi
                     solution = line.split("Solusi: ")[1]
                     knowledge_base[kode_kerusakan]['solution'] = solution.strip()
@@ -54,38 +54,36 @@ def load_knowledge_base_from_file():
         print("Terjadi error saat membaca knowledge base:", e)
     return knowledge_base
 
-# Fungsi untuk melakukan diagnosa
+# Diagnosa Penyakit dari gejala
 def diagnose(gejala_user, knowledge_base):
     hasil_diagnosis = []
     
-    # Iterasi melalui setiap kerusakan dalam knowledge base
     for kode_kerusakan, penyakit_data in knowledge_base.items():
         cf_combine = 0.0
         match_found = False
         
-        # Iterasi melalui setiap gejala untuk penyakit
-        for gejala_code, cf_pakar in penyakit_data['symptoms'].items():
-            # Cek apakah gejala user cocok dengan gejala penyakit
+        for gejala_code, cf_pakar in penyakit_data['diseases'].items():
+           
             cf_pengguna = gejala_user.get(gejala_code, 0.0)
-            if cf_pengguna > 0:  # Hanya jika ada CF dari user
-                match_found = True  # Menandakan ada kecocokan gejala
-                cf_current = hitung_cf(cf_pengguna, cf_pakar['weight'])
+            if cf_pengguna > 0:  # Ada CF User
+                match_found = True  # Gejala ditemukan
+                cf_current = hitung_cf(cf_pengguna, cf_pakar['berat'])
                 cf_combine = combine_cf(cf_combine, cf_current)
 
-        # Hanya tambahkan hasil jika ada gejala yang cocok
+        
         if match_found:
             hasil_diagnosis.append((penyakit_data['name'], cf_combine, penyakit_data['solution'] ))
 
-    # Mengurutkan hasil berdasarkan nilai CF tertinggi
+  
     hasil_diagnosis.sort(key=lambda x: x[1], reverse=True)
     return hasil_diagnosis
 
-# Fungsi untuk memproses input user dari form
+
 def process_user_input(form_data):
     gejala_user = {}
-    for symptom_code in form_data:
-        user_input = int(form_data[symptom_code])
-        gejala_user[symptom_code] = get_cf_pengguna(user_input)
+    for diseases_code in form_data:
+        user_input = int(form_data[diseases_code])
+        gejala_user[diseases_code] = get_cf_pengguna(user_input)
     return gejala_user
 
 @main.route('/')
@@ -94,35 +92,34 @@ def start():
 
 @main.route('/diagnosa')
 def index():
-    # Muat knowledge base dan kumpulkan semua gejala unik
+  
     knowledge_base = load_knowledge_base_from_file()
     if not knowledge_base:
-        return "Error: File knowledge base tidak ditemukan atau kosong."
+        return "Error: Tidak menemukan file knowledge base."
     
-    symptoms = {}
+    diseases = {}
     for penyakit_data in knowledge_base.values():
-        for gejala_code, gejala_data in penyakit_data['symptoms'].items():
-            if gejala_code not in symptoms:
-                symptoms[gejala_code] = gejala_data['name']
-    return render_template('base.html', symptoms=symptoms)
+        for gejala_code, gejala_data in penyakit_data['diseases'].items():
+            if gejala_code not in diseases:
+                diseases[gejala_code] = gejala_data['name']
+    return render_template('base.html', diseases=diseases)
 
 @main.route('/diagnose', methods=['POST'])
 def diagnose_route():
-    # Muat knowledge base
+
     knowledge_base = load_knowledge_base_from_file()
     if not knowledge_base:
-        return "Error: Tidak dapat memuat knowledge base."
+        return "Error: file knowledge base tidak dimuat."
 
-    # Ambil data dari form
+   
     gejala_user = process_user_input(request.form)
 
-    # Lakukan diagnosis berdasarkan gejala
+ 
     hasil_diagnosis = diagnose(gejala_user, knowledge_base)
-    # Filter hasil diagnosis untuk nilai CF > 0
-    hasil_diagnosis_filtered = [diagnosis for diagnosis in hasil_diagnosis if diagnosis[1] > 0]
+ 
+    diagnosis_akhir = [diagnosis for diagnosis in hasil_diagnosis if diagnosis[1] > 0]
 
-    # Render halaman hasil diagnosis
-    return render_template('hasil.html', diagnosis_results=hasil_diagnosis_filtered)
+    return render_template('hasil.html', diagnosis_results=diagnosis_akhir)
 
 if __name__ == '__main__':
     main.run(debug=True)
